c75544c9023cd93ad58019ecb7a4fa5b77ac517d
Fernando Gonzalez <fernandogonzalezvaldz@gmail.com>
2025-09-06 11:56:17 -0400

    .gitignore

--- config-mail.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    'host' => 'mail.fygelectrical.cl',
-    'username' => 'contacto@fygelectrical.cl',
-    'password' => 'Toyota2024&',
-    'port' => 465,
-    'from_email' => 'contacto@fygelectrical.cl',
-    'from_name' => 'FYG Electrical'
-];
